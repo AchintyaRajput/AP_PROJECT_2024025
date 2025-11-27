fb21de7b81d1ead62a5f0d5827463842e2940c0b
wolf-havoc <divij24199@iiitd.ac.in>
2025-11-27 16:08:44 +0530

    Update DropDeadline.java

--- src/main/java/edu/univ/erp/util/DropDeadline.java
+++ src/main/java/edu/univ/erp/util/DropDeadline.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 
 public class DropDeadline {
 
-    // Default deadline (set to any date you want)
+    
     public static LocalDateTime deadline = LocalDateTime.of(2025, 10, 27, 23, 59);
 
     public static void setDeadline(LocalDateTime newDeadline) {
