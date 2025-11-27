3d7c4bc7ce01bcbc2183ae7f3ac3fa701466f0ca
wolf-havoc <divij24199@iiitd.ac.in>
2025-11-27 15:49:46 +0530

    Update TestLogin.java

--- src/main/java/edu/univ/erp/auth/TestLogin.java
+++ src/main/java/edu/univ/erp/auth/TestLogin.java
@@ -6,11 +6,6 @@ public class TestLogin {
     public static void main(String[] args) {
         LoginService loginService = new LoginService();
 
-        // === STEP 1: Register a new test user (run ONCE) ===
-        // ⚠️ Uncomment this line for one run only, then comment again
-//         loginService.registerUser("testuser1", "testpass123", "Instructor");
-
-        // === STEP 2: Try logging in ===
         User user = loginService.login("testuser1", "testpass123");
 
         if (user != null) {
