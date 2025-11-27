fe9653c1f9016be0f912ab2c03f9d64946a2abf6
wolf-havoc <divij24199@iiitd.ac.in>
2025-11-27 15:48:50 +0530

    Update GenerateHash.java

--- src/main/java/edu/univ/erp/auth/GenerateHash.java
+++ src/main/java/edu/univ/erp/auth/GenerateHash.java
@@ -5,7 +5,7 @@ import org.mindrot.jbcrypt.BCrypt;
 public class GenerateHash {
     public static void main(String[] args) {
         String password = "teach123";
-        // change if you want another password
+        
         String hash = BCrypt.hashpw(password, BCrypt.gensalt());
         System.out.println("BCrypt hash for " + password + ":");
         System.out.println(hash);
